122629e86ed5be69c1e269092f36eece3cff84b1
ZTRCOMPANYs <ztremcompany@gmail.com>
2024-08-24 21:10:33 -0300

    Add files via upload

---
+++ Jogos/jogos.js
@@ -0,0 +1,5 @@
+// Exemplo de um script simples (opcional)
+// Você pode adicionar comportamentos interativos ao seu site aqui
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Página carregada e pronta!");
+});
